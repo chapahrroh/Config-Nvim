acd0c8bb5e8b298caf667c548e31ce2727051e1f
chapahrroh <chapahrroh@gmail.com>
2023-01-10 17:26:40 -0300

    configuracion inicial con LUA

---
+++ lua/core/configuracion.lua
@@ -0,0 +1,16 @@
+--Abrebiaciones--
+
+local cmd  = vim.cmd
+local exec = vim.api.nvim_exec
+local fn   = vim.fn
+local opt  = vim.opt
+
+--General--
+
+opt.number         = true
+opt.relativenumber = true
+opt.mouse          = a
+--opt.encoding       = UTF-8
+opt.cursorline     = true
+--opt.colorcolumn    = true
+
---
+++ lua/core/mapeoTeclas.lua
@@ -0,0 +1,16 @@
+--Abrebiaciones--
+
+local map         = vim.api.nvim_set_keymap
+local default_ops = {noremap = true, silent = true}
+local cmd         = vim.cmd
+local g           = vim.g
+
+--Tecla lider--
+
+g.mapleader = " "
+
+--Comandos Generales--
+
+map('n',  '<leader>w',  ':w<CR>', default_ops)
+map('n',  '<leader>q',  ':q<CR>', default_ops)
+map('n', '<leader>ss', ':q!<CR>', default_ops) 
